205f0ac5de11bbeaa2c87752aefca3cca6917d33
Richard Parnaby-King <richard.parnaby-king@clickconsult.com>
2014-01-10 14:18:23 +0000

    Updated plugin. Logo updated Added packed Chrome file for easy install

--- background.js
+++ background.js
@@ -1,3 +1,7 @@
+/*
+ * @copyright: Richard Parnaby-King
+ * @url: http://richard.parnaby-king.co.uk
+ */
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.executeScript(null, {file: "contentscript.js"});
 });
--- contentscript.js
+++ contentscript.js
@@ -1,10 +1,16 @@
+/*
+ * Add a red border around external non-nofollowed links.
+ * @copyright: Richard Parnaby-King
+ * @url: http://richard.parnaby-king.co.uk
+ */
+//get all links that contain 'http', are not links to the current host, and do not contain the rel="nofollow" attribute
 var result = document.evaluate("//a[contains(@href,\"http\") and not(contains(@href, \"" + location.host + "\")) and not(contains(@rel, \"nofollow\"))]",
 																document.documentElement,
 																null,
 																XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
 
+//add a red border around each link
 for (var i=0; i<result.snapshotLength; i++) {
-	//console.log(result.snapshotItem(i).innerHTML);
 	result.snapshotItem(i).style.border='1px dashed red';
 }
 
